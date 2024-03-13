077274c950a77e9d0b0b14c800af93e0d97a8bad
MarkRBishop <mrb.8@hotmail.com>
2024-03-13 14:13:57 -0500

    added Schemas folder and files

---
+++ server/Schemas/typeDefs.js
@@ -0,0 +1,58 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  # Define your GraphQL types here
+  type User {
+    _id: ID
+    username: String
+    email: String
+    password: String
+    savedBooks: [Book]
+  }
+
+  type Book {
+    bookId: ID
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  input savedBooks{
+    id: ID
+    bookId: ID
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+
+  type Query {
+    me: User
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(bookData: BookInput!): User
+    removeBook(bookId: ID!): User
+  }
+
+  input BookInput {
+    authors: [String]
+    description: String
+    title: String
+    bookId: ID
+    image: String
+    link: String
+  }
+`;
+
+module.exports = typeDefs;
